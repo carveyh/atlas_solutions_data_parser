271a6b84bba9314f2c0929ed30f1e2db375e17a6
Carvey Hor <carveyh.dev@gmail.com>
2024-08-20 10:08:31 -0400

    Clean up code, add instructions to read me

--- solutions/with_dependency_solution.rb
+++ solutions/with_dependency_solution.rb
@@ -1,5 +1,21 @@
-require "csv"
 require "benchmark"
+require "csv"
+
+=begin
+* Assumptions:
+- Spaces used as delimiters only, e.g. soccer.dat uses underscore for team names
+- Columns contain the same, valid data type
+- HEADER ROW: exactly one header row, with column names beginning with alphabetical character
+- DATA ROWS: begin with numeric digit
+
+* Simple solution:
+This solution is simpler than the no_dependency_solution, using Ruby's CSV class to convert data into an Array of Hashes.
+Since the Ruby's CSV :headers option uses the first row as headers, not to mention varying white-space delimited data
+with gaps and extra non-data characters, it still must be cleaned.
+
+Unlike the other solution, it does not attempt to match cells to the proper column, resulting in some mismatches.
+However, for the scope of the prompts, it still produces the correct results.
+=end
 
 # Helper method in case table missing id col name
 def generate_unique_id_col_name(col_names_arr)
@@ -8,14 +24,15 @@ def generate_unique_id_col_name(col_names_arr)
   "ID-#{num}"
 end
 
+
 # Convert varying-space-delimited values to CSV: 
-# - consolidate spaces and convert them into comma delimiters, keep only [\w. ] characters,
+# - consolidate spaces and convert them into comma delimiters, keep only /[\w. ]/ characters,
 # - isolate header row and data rows only
 def clean_data(filename)
   raw_content = File.read(filename).split("\n")
   formatted_content = raw_content.map {|row| row.strip.gsub(/[^\w. ]*/, "").gsub(/ +/, ",")}
 
-  # header is first non-numeric-leading row before first numeric-leading row
+  # header is first alphabetic-leading row before first numeric-leading row
   first_data_row_idx = formatted_content.index{|row| row.match?(/^\s*\d/)}
   header_idx = first_data_row_idx
   header_idx -= 1 until formatted_content[header_idx].match?(/^\s*[a-zA-Z]/)
@@ -28,10 +45,14 @@ def clean_data(filename)
     header_and_data_only[0] = generate_unique_id_col_name(header_string.split(",")) + "," + header_string
   end
 
-  # add data rows, skip any lines that don't begin with an id number
+  # extract data rows, skip any lines that don't begin with an id number, return as string
   header_and_data_only.concat(formatted_content.filter{|row| row.match?(/^\s*\d+/)}).join("\n")
 end
 
+
+# Given a whitespace-delimited data file,
+# identify the row with the smallest difference between two specified columns, 
+# and return the value of a third specified column from the identified row.
 def min_range(filename, max_col, min_col, select_col)
   csv_data = clean_data(filename)
   parsed_data = CSV.parse(csv_data, headers: :first_row).map(&:to_h)
@@ -47,15 +68,6 @@ def min_range(filename, max_col, min_col, select_col)
   parsed_data[min_range_idx][select_col]
 end
 
-
-
-# fileword = "customers-100.csv"
-# fileword = "soccer.dat"
-# fileword = "w_data.dat"
-# filename = File.expand_path("../../data/#{fileword}", __FILE__)
-
-# puts min_range(filename, "F", "A", "Team")
-# puts min_range(filename, "MxT", "MnT", "Dy")
 
 puts "Solution with CSV Class"
 puts "==========================================="
